421651d83aab1c56a4f6f17729013f8a94552abe
Javokhirbek <khaydaraliev99@gmail.com>
2021-11-29 23:08:15 +0100

    Heroku Deployment

--- app/settings.py
+++ app/settings.py
@@ -9,15 +9,14 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import environ
+
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env();
-environ.Env.read_env();
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -207,3 +206,6 @@ EMAIL_PORT=587
 DEFAULT_FROM_EMAIL=os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
+
+
+django_heroku.settings(locals())
